54d85b94820b70a7a4ed1febc5ada445b6381885
jbagger <jbagger@student.hive.fi>
2022-11-14 18:09:07 +0200

    still a wip

--- get_next_line.c
+++ get_next_line.c
@@ -5,63 +5,55 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-/*
-	1. Read onto "buf"
-	2. While isn't NULL, copy "buf" to "tmp"
-	3. 
+size_t BUF_SIZE = 4;
 
-*/
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+char *check_for_line(char *str)
 {
-	size_t	i;
-
-	i = 0;
-	if (dstsize > 0)
+	while (str)
 	{
-		while (src[i] && i < dstsize - 1)
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
+		if (*str == '\n')
+			return (0);
+		str++;
 	}
-	while (src[i])
-		i++;
-	return (i);
+	return (1);
 }
 
 char *get_next_line(int fd)
 {
-	int BUFFER_SIZE = 5;
-	char buf[BUFFER_SIZE];
-	static char *tmp;
-	char *line;
-	size_t i;
+	char **str_array;
+	char buffer[BUF_SIZE];
 	size_t bytes_read;
+	size_t i;
+	size_t n;
+	int boolean;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &line, 0) < 0)
-		return (NULL);
-	bytes_read = 1;
-	line = NULL;
-
-	// check how much is read
-	bytes_read = read(fd, buf, BUFFER_SIZE);
-
-	printf("Test%zu\n", bytes_read);
-	
-	// copy buf -> tmp
-	ft_strlcpy(tmp, buf, bytes_read);
-	
-	// copy tmp -> line
-	i = 0;
-	while (tmp[i] && tmp[i] != '\n')
+	bytes_read = read(fd, buffer, BUF_SIZE);
+	boolean = 0;
+	n = 0;
+	// read until newline or end is reached
+	while (bytes_read >= 0 && boolean == 0)
 	{
-		line[i] = tmp[i];
-		i++;
+		i = 0;
+		while (i < bytes_read)
+		{
+			str_array[n][i] = buffer[i];
+			i++;
+		}
+		str_array[i] = '\0';
+		n++;
+		bytes_read = read(fd, buffer, BUF_SIZE);
+		if (check_for_newline(str_array[i]))
+		{
+			while (str_array[i])
+			{
+				
+			}
+			boolean = 1;
+		}
 	}
-	if (tmp[i] == '\n')
-		line[i] = tmp[i];
+
+	
+	
 	return (line);
 }
 
